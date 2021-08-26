6eb3f524f41f6a3ccb8fbc4fa97b1e68e56ac519
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-08-26 22:57:58 +0530

    Added:-Condation is added

--- Emp.js
+++ Emp.js
@@ -1,28 +1,34 @@
 
  
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 20;
-
-function getWorkinghours(empCheck){
-    switch(empCheck){
-        case IS_PART_TIME:
-            return PART_TIME_HOURS;
-        case IS_FULL_TIME:
-            return FULL_TIME_HOURS;
-        default:
-            return 0;
-    }
-}
-
-let empHrs = 0;
-for (let day = 0; day<NUM_OF_WORKING_DAYS; day++){
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkinghours(empCheck);
-}
-
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Employee Hours= " +empHrs+ ":" + "Total Employee Wage =" +empWage);
+ const IS_PART_TIME = 1;
+ const IS_FULL_TIME = 2;
+ const PART_TIME_HOURS = 4;
+ const FULL_TIME_HOURS = 8;
+ const WAGE_PER_HOUR = 20;
+ const NUM_OF_WORKING_DAYS = 20;
+ const MAX_HOURS_IN_MONTH = 100;
+ 
+ function getWorkinghours(empCheck){
+     switch(empCheck){
+         case IS_PART_TIME:
+             return PART_TIME_HOURS;
+         case IS_FULL_TIME:
+             return FULL_TIME_HOURS;
+         default:
+             return 0;
+     }
+ }
+ 
+ let totalEmpHrs = 0;
+ let totalWorkingDays = 0;
+ while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+     totalWorkingDays++;
+     let empCheck = Math.floor(Math.random() * 10) % 3;
+     totalEmpHrs += getWorkinghours(empCheck);
+ }
+ 
+ let empWage = totalEmpHrs * WAGE_PER_HOUR;
+ 
+ console.log("Total Working days: " +totalWorkingDays);
+ console.log("Total Employee Hours= " +totalEmpHrs);
+ console.log("Total Employee Wage =" +empWage);
